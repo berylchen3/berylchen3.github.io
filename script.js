79d418731e9a9ea1a90a32832ecdb4bb1c4b004c
berylchen3 <u1388485@umail.utah.edu>
2024-11-17 00:26:31 -0700

    Add a Photo to the Website and Make Changes to Background and Text

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    title.style.background = 'linear-gradient(90deg, #6a5acd, #48b1f3)';
+    title.style.webkitBackgroundClip = 'text';
+    title.style.webkitTextFillColor = 'transparent';
+});
