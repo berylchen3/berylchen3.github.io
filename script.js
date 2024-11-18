30cee4cdc795fc93d1963f9b58173e8652f9c1f7
berylchen3 <u1388485@umail.utah.edu>
2024-11-18 01:25:26 -0700

    Update projects.html
    Make the color more visible and add link to my project

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     title.style.webkitBackgroundClip = 'text';
     title.style.webkitTextFillColor = 'transparent';
 });
+
+
+function showModal(projectId) {
+    document.getElementById(`${projectId}-modal`).style.display = 'block';
+}
+
+function closeModal(projectId) {
+    document.getElementById(`${projectId}-modal`).style.display = 'none';
+}
+
+//Close modal when clicking outside
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let modal of modals) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+};
